8086a5fe2ac7768a906bcc007e263f50e45841cb
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-05 21:04:53 -0300

    implente switch logre implementar SWITCH - tuve error en linea 8 (posicionLlegada = (dato.value))

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 rotulo.innerHTML = "Ingrese la posición de llegada del competidor: ";
 
-let posicionLlegada: number = dato.value;
+let dato = document.getElementById("dato");
+let btnverif = document.getElementById("btnverif");
+
 btnverif.addEventListener("click", () => {
-  let posicionLlegada: number = dato.value;
-  if (posicionLlegada == 1) {
-    console.log("Entregar medalla de oro");
-  } else {
-    if (posicionLlegada == 2) {
+  let posicionLlegada: number = Number(dato.value);
+  switch (posicionLlegada) {
+    case 1:
+      posicionLlegada === 1;
+      console.log("Entregar medalla de oro");
+      break;
+    case 2:
+      posicionLlegada === 2;
       console.log("Entregar medalla de plata");
-    } else {
-      if (posicionLlegada == 3) {
-        console.log("Entregar medalla de bronce");
-      } else {
-        console.log("Entregar mención de participación");
-      }
-    }
+      break;
+    case 3:
+      posicionLlegada === 3;
+      console.log("Entregar medalla de bronce");
+      break;
+    default:
+      console.log("Entregar mención de participación");
   }
 });
